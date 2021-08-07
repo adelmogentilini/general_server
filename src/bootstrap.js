077c5db4e414d8223301dae9dadec11bf2262b60
Adelmo Gentilini <gentilini@xdatasrl.it>
2021-08-07 15:09:01 +0200

    commit controllo git status

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,15 +3,13 @@ const { bootstrapApp } = require('./_coreboot');
 
 function main() {
 
-  setTimeout(function () {
+  setInterval(function () {
     require('child_process').exec('git pull ', (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         return;
       }
       let gitresp = stdout
-      console.log(`${stdout}`);
-      console.error(`${stderr}`);
       if(gitresp.indexOf("Already up to date") > -1)
       {
         console.log( "Controllo versione OK")
@@ -26,7 +24,7 @@ function main() {
         })
 
       }
-    }, 2000);
+    }, 3000);
   })
   return bootstrapApp();
 
