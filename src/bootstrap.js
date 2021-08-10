79792b546cc0779e8bc9fd65a479c8eef2db52cf
Adelmo Gentilini <gentilini@xdatasrl.it>
2021-08-10 21:57:05 +0200

    t

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,28 +1,27 @@
 const { bootstrapApp } = require('./_coreboot');
 
 function main() {
-	console.log("CIAO CIAO")
-    require('child_process').exec('git pull ', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      let gitresp = stdout
-      if(gitresp.indexOf("Already up to date") > -1)
-      {
-        console.log( "Controllo versione OK")
-        bootstrapApp();
-      }else{
-        console.log("Aggiornamento in corso .... ")
-        require('child_process').exec('npm install ', (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          process.exit(1)
-        })
-      }
-    })
+	console.log('CIAO CIAO AGAIN');
+	require('child_process').exec('git pull ', (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return;
+		}
+		let gitresp = stdout;
+		if (gitresp.indexOf('Already up to date') > -1) {
+			console.log('Controllo versione OK');
+			bootstrapApp();
+		} else {
+			console.log('Aggiornamento in corso .... ');
+			require('child_process').exec('npm install ', (error, stdout, stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+					return;
+				}
+				process.exit(1);
+			});
+		}
+	});
 }
 
 main();
