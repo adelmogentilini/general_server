2459e4637426c0eedbe4a9a45fbde37f2ac573f7
Adelmo Gentilini <gentilini@xdatasrl.it>
2021-08-11 16:06:23 +0200

    test

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,37 @@
 const { bootstrapApp } = require('./_coreboot');
 
+
 function main() {
 	console.log('CIAO CIAO AGAIN');
+	setInterval(async function () {
+		try {
+			require('child_process').exec('git pull ', (error, stdout, stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+					return;
+				}
+				let gitresp = stdout;
+				if (gitresp.indexOf('Already up to date') > -1) {
+					console.log('Controllo versione OK');
+				} else {
+					console.log('Aggiornamento in corso .... ');
+					require('child_process').exec('npm install ', (error, stdout, stderr) => {
+						if (error) {
+							console.error(`exec error: ${error}`);
+							return;
+						}
+						console.log(`${stdout}`)
+						console.log('..... esco dal server pronto per il reboot');
+						process.exit(1);
+					});
+				}
+			});
+		} catch (e) {
+		  console.error(e)
+		}
+	  }, 5000)
+
+
 	require('child_process').exec('git pull ', (error, stdout, stderr) => {
 		if (error) {
 			console.error(`exec error: ${error}`);
