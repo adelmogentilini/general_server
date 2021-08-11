7d97b287009bd07e260be426cf34816183f11047
Adelmo Gentilini <gentilini@xdatasrl.it>
2021-08-11 16:38:18 +0200

    status

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,8 @@
 const { bootstrapApp } = require('./_coreboot');
 
-
 function main() {
-	console.log('Deploy release 1');
-	setInterval(async function () {
+	console.log('CIAO CIAO AGAIN');
+	setInterval(async function() {
 		try {
 			require('child_process').exec('git pull ', (error, stdout, stderr) => {
 				if (error) {
@@ -20,38 +19,26 @@ function main() {
 							console.error(`exec error: ${error}`);
 							return;
 						}
-						console.log(`${stdout}`)
+						console.log(`${stdout}`);
 						console.log('..... esco dal server pronto per il reboot');
 						process.exit(1);
 					});
 				}
 			});
 		} catch (e) {
-		  console.error(e)
+			console.error(e);
 		}
-	  }, 20000)
+	}, 15000);
 
-
-	require('child_process').exec('git pull ', (error, stdout, stderr) => {
+	require('child_process').exec('git status ', (error, stdout, stderr) => {
 		if (error) {
 			console.error(`exec error: ${error}`);
 			return;
 		}
-		let gitresp = stdout;
-		if (gitresp.indexOf('Already up to date') > -1) {
-			console.log('Controllo versione OK');
-			console.log(`${stdout}`)
-			bootstrapApp();
-		} else {
-			console.log('Aggiornamento in corso .... ');
-			require('child_process').exec('npm install ', (error, stdout, stderr) => {
-				if (error) {
-					console.error(`exec error: ${error}`);
-					return;
-				}
-				process.exit(1);
-			});
-		}
+		console.log("***********************************************************")
+		console.log(`${stdout}`);
+		console.log("***********************************************************")
+		bootstrapApp();
 	});
 }
 
